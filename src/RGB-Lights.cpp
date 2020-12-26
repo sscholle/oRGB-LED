c312f6da6d7738cf9bdcbaf383aef476c95e8b74
Sebastian Scholle <sscholle@gmail.com>
2020-12-27 01:36:24 +0200

    oRGB LED Library created

--- src/RGB-Lights.cpp
+++ src/RGB-Lights.cpp
@@ -1,16 +1,10 @@
 /*
-  Fade
+  oRGB - LED Sketch
 
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
+  Red, Green and Blue LED stripas can fade into different colours. Like a Rainbow,
+  but more BRILLIANT! It 
+  
+  Seee github for more info: https://github.com/sscholle/oRGB-LED
 */
 #include <Arduino.h>
 
@@ -19,20 +13,20 @@ const uint8_t RED_LED = 10;
 const uint8_t BLUE_LED = 11;
 
 
-uint8_t SAME_COLOR_ITERATIONS = 12;
-uint8_t GENERAL_DELAY = 4;
+uint8_t SAME_COLOR_ITERATIONS = 4;
+uint8_t GENERAL_DELAY = 3;
 uint8_t MAX_BRIGHTNESS = 64;
-uint8_t MIN_BRIGHTNESS = 4;
+uint8_t MIN_BRIGHTNESS = 2;
 
 
 uint8_t G_BRIGHT = MIN_BRIGHTNESS;
 uint8_t G_FADEAMNT = 1;
 
-uint8_t R_BRIGHT = MIN_BRIGHTNESS;
-uint8_t R_FADEAMNT = 2;
+uint8_t R_BRIGHT = 32;
+uint8_t R_FADEAMNT = 1;
 
-uint8_t B_BRIGHT = MIN_BRIGHTNESS;
-uint8_t B_FADEAMNT = 3;
+uint8_t B_BRIGHT = MAX_BRIGHTNESS;
+uint8_t B_FADEAMNT = 1;
 
 void writeRed(uint8_t br){
   analogWrite(RED_LED, br);
